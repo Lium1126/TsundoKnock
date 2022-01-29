e0afbb7c11eefefcc6c1ff4649ae13b46b79ecf4
Lium1126 <yosi.4sya@gmail.com>
2022-01-29 18:12:18 +0900

    minor fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -94,75 +94,79 @@
                     <script>
                     $(function() {
                         $('#isbn').on('input', function() {
-                            var json = $.getJSON('https://api.openbd.jp/v1/get?isbn=' + $('#isbn').val(), function(json) {
-                                if (json[0] != null) {
-                                    /*--- Create preview table ---*/
-                                    const preview_table = $('<table><tbody>');
-                                    preview_table.addClass('m-auto');
-                                    preview_table.addClass('lead');
+                            if ($('#isbn').val().length == 0) {
+                                $('#preview').html("");
+                            } else {
+                                var json = $.getJSON('https://api.openbd.jp/v1/get?isbn=' + $('#isbn').val(), function(json) {
+                                    if (json[0] != null) {
+                                        /*--- Create preview table ---*/
+                                        const preview_table = $('<table><tbody>');
+                                        preview_table.addClass('m-auto');
+                                        preview_table.addClass('lead');
 
-                                    const title_preview_row = $('<tr></tr>');
-                                    const title_preview = $('<td></td>');
-                                    title_preview.attr('id', 'title_preview');
-                                    title_preview.attr('colspan', '2');
-                                    title_preview.addClass('p-2');
-                                    title_preview.css('background-color', '#dcdcdc');
-                                    title_preview.appendTo(title_preview_row);
-                                    title_preview_row.appendTo(preview_table);
+                                        const title_preview_row = $('<tr></tr>');
+                                        const title_preview = $('<td></td>');
+                                        title_preview.attr('id', 'title_preview');
+                                        title_preview.attr('colspan', '2');
+                                        title_preview.addClass('p-2');
+                                        title_preview.css('background-color', '#dcdcdc');
+                                        title_preview.appendTo(title_preview_row);
+                                        title_preview_row.appendTo(preview_table);
 
-                                    const book_info_row = $('<tr></tr>');                                    
-                                    const cover_preview_col = $('<td></td>');
-                                    cover_preview_col.addClass('p-2');
-                                    cover_preview_col.css('width', '224px');                                    
-                                    const cover_preview = $('<img>');
-                                    cover_preview.attr('id', 'cover_preview');
-                                    cover_preview.css('width', '200');
-                                    cover_preview.css('border', '1px solid');
-                                    cover_preview.appendTo(cover_preview_col);
-                                    cover_preview_col.appendTo(book_info_row);
+                                        const book_info_row = $('<tr></tr>');                                    
+                                        const cover_preview_col = $('<td></td>');
+                                        cover_preview_col.addClass('p-2');
+                                        cover_preview_col.css('width', '224px');                                    
+                                        const cover_preview = $('<img>');
+                                        cover_preview.attr('id', 'cover_preview');
+                                        cover_preview.css('width', '200');
+                                        cover_preview.css('border', '1px solid');
+                                        cover_preview.appendTo(cover_preview_col);
+                                        cover_preview_col.appendTo(book_info_row);
 
-                                    const author_preview_col = $('<td></td>');
-                                    author_preview_col.addClass('p-2');
-                                    author_preview_col.addClass('align-middle');
-                                    const author_preview = $('<p></p>');
-                                    author_preview.attr('id', 'author_preview');
-                                    author_preview.appendTo(author_preview_col);
-                                    const publisher_preview = $('<p></p>');
-                                    publisher_preview.attr('id', 'publisher_preview');
-                                    publisher_preview.appendTo(author_preview_col);
-                                    author_preview_col.appendTo(book_info_row);
-                                    book_info_row.appendTo(preview_table);
+                                        const author_preview_col = $('<td></td>');
+                                        author_preview_col.addClass('p-2');
+                                        author_preview_col.addClass('align-middle');
+                                        const author_preview = $('<p></p>');
+                                        author_preview.attr('id', 'author_preview');
+                                        author_preview.appendTo(author_preview_col);
+                                        const publisher_preview = $('<p></p>');
+                                        publisher_preview.attr('id', 'publisher_preview');
+                                        publisher_preview.appendTo(author_preview_col);
+                                        author_preview_col.appendTo(book_info_row);
+                                        book_info_row.appendTo(preview_table);
 
-                                    $('</tbody></table>').appendTo(preview_table);
-                                    $('#preview').html(preview_table);
-                                    /*-------*/
+                                        $('</tbody></table>').appendTo(preview_table);
+                                        $('#preview').html(preview_table);
+                                        /*-------*/
 
-                                    /*--- Set value ---*/
-                                    $('#title_preview').html(json[0]["summary"]["title"]);
-                                    $('#title').val(json[0]["summary"]["title"]);
-                                    const cover_url = (json[0]["summary"]["cover"].length == 0 ? './templates/noimage.png' : json[0]["summary"]["cover"]);
-                                    $('#cover_preview').attr('src', cover_url);
-                                    $('#cover').val(cover_url);
-                                    $('#author_preview').html(json[0]["summary"]["author"]);
-                                    $('#publisher_preview').html(json[0]["summary"]["publisher"]);
-                                    /*------*/
+                                        /*--- Set value ---*/
+                                        $('#title_preview').html(json[0]["summary"]["title"]);
+                                        $('#title').val(json[0]["summary"]["title"]);
+                                        const cover_url = (json[0]["summary"]["cover"].length == 0 ? './templates/noimage.png' : json[0]["summary"]["cover"]);
+                                        $('#cover_preview').attr('src', cover_url);
+                                        $('#cover').val(cover_url);
+                                        $('#author_preview').html(json[0]["summary"]["author"]);
+                                        $('#publisher_preview').html(json[0]["summary"]["publisher"]);
+                                        /*------*/
 
-                                    /*--- Activate submit button ---*/
-                                    $('#addbtn').prop('disabled', false);
-                                    /*------*/
-                                } else {
-                                    const msg = $('<p>');
-                                    msg.addClass('lead');
-                                    msg.addClass('text-center');
-                                    msg.append("Book's information does not found");
-                                    $('</p>').appendTo(msg);
-                                    $('#preview').html(msg);
+                                        /*--- Activate submit button ---*/
+                                        $('#addbtn').prop('disabled', false);
+                                        /*------*/
+                                    } else {
+                                        const msg = $('<p>');
+                                        msg.addClass('lead');
+                                        msg.addClass('text-center');
+                                        msg.append("Book's information does not found");
+                                        $('</p>').appendTo(msg);
+                                        $('#preview').html(msg);
 
-                                    /*--- Disable submit button ---*/
-                                    $('#addbtn').prop('disabled', true);
-                                    /*------*/
-                                }
-                            });
+                                        /*--- Disable submit button ---*/
+                                        $('#addbtn').prop('disabled', true);
+                                        /*------*/
+                                    }
+                                });
+                            }
                         });
                     });
                     </script>
